e0f3b5899dfb832947642f624437c807c58a4225
ahmed-abdi1991 <squarehajji@gmail.com>
2023-11-01 23:18:22 +0300

    assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -91,3 +91,15 @@ function QuestionForm(props) {
 }
 
 export default QuestionForm;
+fetch("http://localhost:4000/questions", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    "prompt": formData.prompt,
+    "answers": [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+    "correctIndex": parseInt(formData.correctIndex, 10)
+  })
+}).then(resp => resp.json())
+  .then(data => onSubmitQuestion(data))
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -5,6 +5,7 @@ function QuestionList() {
     <section>
       <h1>Quiz Questions</h1>
       <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{listQuestions}</ul>
     </section>
   );
 }
